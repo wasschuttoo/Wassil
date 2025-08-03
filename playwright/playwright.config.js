142004696a64d87a3b6df998fe36312226a6e96a
Wass <chelsea.fc.wass@hotmail.com>
2025-08-03 11:37:11 +0400

    fix playwright test run on github

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -1,18 +1,18 @@
-// playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
+
 const config = {
   testDir: './tests',
   timeout: 30000,
   retries: 0,
-  outputDir: 'test-results/',
+  outputDir: './test-results/',
   use: {
+    browserName: 'chromium',
     headless: false,
     viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
-    baseURL: 'http://localhost:3000', // Change if needed
+    baseURL: 'https://wasschuttoo.github.io/Wassil', // Change if needed
   },
 };
 
